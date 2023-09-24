98fc57bce9aab82d6a347f4065f750b521d42d19
Jonathan Spruance <jspruance@inflightcorp.com>
2023-09-23 18:04:30 -0700

    Update token code

--- contracts/OceanToken.sol
+++ contracts/OceanToken.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OceanToken is ERC20 {
     constructor() ERC20("OceanToken", "OCT") {
-        _mint(msg.sender, 1000 * (10 ** decimals()));
+        _mint(msg.sender, 1000000 * (10 ** decimals()));
     }
 }
